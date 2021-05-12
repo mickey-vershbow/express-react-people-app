3713f6b4ba51d39b81a2247bc90fecdbf19daccf
Mickey Vershbow <mickey.vershbow@gmail.com>
2021-05-11 20:31:00 -0400

    seeded mongo with starter data

--- routes/home.js
+++ routes/home.js
@@ -15,6 +15,37 @@ const People = require("../Models/People");
 const { restart } = require("nodemon");
 
 ///////////////////////////////
+// SEED DATA
+////////////////////////////////
+const peopleArr = [
+  {
+    name: "Cate Blanchett",
+    image:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.EeBxvHj7mq2pFE6h1Eoy7wHaJZ%26pid%3DApi&f=1",
+    title: "actress",
+  },
+  {
+    name: "Kate Winslet",
+    image:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.ZUKsaf0HtlEmuiwZrImAWgHaLH%26pid%3DApi&f=1",
+    title: "actress",
+  },
+  {
+    name: "Olivia Coleman",
+    image:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.nXOazAh81u-Wmn1xSVC52AHaOG%26pid%3DApi&f=1",
+    title: "actress",
+  },
+  {
+    name: "Regina King",
+    image:
+      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.EfSSlYJdApYI817iWYF0rwHaLH%26pid%3DApi&f=1",
+    title: "actress",
+  },
+];
+
+
+///////////////////////////////
 // ROUTES
 ////////////////////////////////
 
@@ -67,6 +98,20 @@ router.post("/people", async (req, res) => {
     //send error
     res.status(400).json(error);
   }
+});
+
+//seed route
+router.get("/seed", (req, res) => {
+  People.collection.drop();
+  People.create(peopleArr, (error, person) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(peopleArr);
+      res.json(peopleArr);
+    }
+    db.close();
+  });
 });
 
 ///////////////////////////////
